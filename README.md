dc9f48f049a1528382cca4638b594c417e5ead72
ibmpariwesh <ibmpariwesh@gmail.com>
2022-05-31 16:10:58 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Playing with CQRS and Event Sourcing in Spring Boot and Axon
+Sample code for Playing with CQRS and Event Sourcing in Spring Boot and Axon tutorial
+
+
+mvn package
+java -jar AxonServer\axonserver.jar
+start axon server
+
+http://localhost:8080/swagger-ui.html#/
+
+http://localhost:8080/v2/api-docs
---
+++ src/main/java/com/targa/labs/dev/cqrses/service/AccountCommandService.java
@@ -0,0 +1,49 @@
+package com.targa.labs.dev.cqrses.service;
+
+import com.targa.labs.dev.cqrses.command.CreateAccountCommand;
+import com.targa.labs.dev.cqrses.command.CreditMoneyCommand;
+import com.targa.labs.dev.cqrses.command.DebitMoneyCommand;
+import com.targa.labs.dev.cqrses.entity.BankAccount;
+import com.targa.labs.dev.cqrses.rest.dto.AccountCreationDTO;
+import com.targa.labs.dev.cqrses.rest.dto.MoneyAmountDTO;
+import lombok.AllArgsConstructor;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import static com.targa.labs.dev.cqrses.service.ServiceUtils.formatUuid;
+
+@Service
+@AllArgsConstructor
+public class AccountCommandService {
+    private final CommandGateway commandGateway;  //RestTemplate
+
+    public CompletableFuture<BankAccount> createAccount(AccountCreationDTO creationDTO) {
+    	//logic
+//    	insert db
+        return this.commandGateway.send(new CreateAccountCommand(
+                UUID.randomUUID(),
+                creationDTO.getInitialBalance(),
+                creationDTO.getOwner()
+        ));
+        //send mail?? -> separate class for reuse
+    }
+
+    public CompletableFuture<String> creditMoneyToAccount(String accountId,
+                                                          MoneyAmountDTO moneyCreditDTO) {
+        return this.commandGateway.send(new CreditMoneyCommand(
+                formatUuid(accountId),
+                moneyCreditDTO.getAmount()
+        ));
+    }
+
+    public CompletableFuture<String> debitMoneyFromAccount(String accountId,
+                                                           MoneyAmountDTO moneyDebitDTO) {
+        return this.commandGateway.send(new DebitMoneyCommand(
+                formatUuid(accountId),
+                moneyDebitDTO.getAmount()
+        ));
+    }
+}
